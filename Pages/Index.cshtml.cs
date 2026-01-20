ccaf3c15a610b52a19a8bfe759a17846d119572c
devsantiag <contatofasantiago@hotmail.com>
2026-01-19 21:55:38 -0300

    Study: realizando aplicações aleatórias para o entendimento da Linguagem.
    Aprendizado hoje:
    
    - Criação da Button com Link
    - Criação de sistema de validação de dados

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,13 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace HelloWorld.Pages
 {
     public class IndexModel : PageModel
     {
+
+        [BindProperty]
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        [StringLength(50, MinimumLength = 7, ErrorMessage = "O nome deve ter entre 7 e 50 caracteres")]
+
+        public string Name { get; set; }
+        public string Message { get; set; }
+
         public void OnGet()
         {
+            Message = "Qual seu nome? ";
+        }
 
+        public void OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                Message = "Por favor, corrija os erros abaixo.";
+                return;
+            }
+         
+            Message = $"Seja bem-vindo, {Name}!";
         }
     }
 }
